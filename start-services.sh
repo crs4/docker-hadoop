a876e5a545281883669beea29bee1703d7e2338e
Marco Enrico Piras <kikkomep@crs4.it>
2015-09-04 01:04:40 +0200

    Added utility script for starting a dockerized Hadoop by means of the DockerCompose orchestrator

---
+++ start-services.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# check the number of arguments
+if [[ "$#" -ne 2 ]]; then
+  echo -e "\n *** Invalid number of arguments: provide DOCKERHUB_REPOSITORY and HADOOP_VERSION \n"
+  exit -1 
+fi
+
+# set repository and hadoop version from arguments
+DOCKERHUB_REPOSITORY=${1}
+HADOOP_VERSION=${2}
+
+# set compose project name
+DOCKER_COMPOSE_PROJECT="" #"${HADOOP_VERSION}"
+
+# current path
+CURRENT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+# buid the docker-compose file
+${CURRENT_PATH}/build-compose.sh ${DOCKERHUB_REPOSITORY} ${HADOOP_VERSION}
+
+# start compose
+docker-compose up -d # no-project name
