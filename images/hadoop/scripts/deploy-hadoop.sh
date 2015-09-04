ecd890d615d0cdbc8973af80ca10a485da2333f6
Marco Enrico Piras <kikkomep@crs4.it>
2015-09-04 03:09:56 +0200

    hadoop deployer checks whether the tar.gz exists

--- images/hadoop/scripts/deploy-hadoop.sh
+++ images/hadoop/scripts/deploy-hadoop.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 # check the number of arguments
-if [[ "$#" -ne 3 ]]; then
-  echo -e "\n *** Invalid number of arguments: provide TAR.GZ path, HADOOP_VERSION and HADOOP_HOME \n"
+if [[ "$#" -ne 4 ]]; then
+  echo -e "\n *** Invalid number of arguments: provide TAR.GZ directory, TAR.GZ filename, HADOOP_VERSION and HADOOP_HOME \n"
   exit -1 
 fi
 
 # Set version and destination from args
-HADOOP_TARGZ=${1}
-HADOOP_VERSION=${2}
-HADOOP_HOME=${3}
+HADOOP_TARGZ_DIR=${1}
+HADOOP_TARGZ_FILE=${1}
+HADOOP_TARGZ_PATH=${1}/${2}
+HADOOP_VERSION=${3}
+HADOOP_HOME=${4}
+
+# Download the archive if doesn't exists!
+if [[ ! -f "${HADOOP_TARGZ_PATH}" ]]; then
+  download-hadoop.sh ${HADOOP_VERSION}
+fi
 
 # Expand hadoop archive and move it to the '${HADOOP_HOME}' folder
-cd ${HADOOP_TARGZ}
-tar xzvf hadoop-${HADOOP_VERSION}.tar.gz 
+tar xzvf ${HADOOP_TARGZ_PATH} 
 mv hadoop-${HADOOP_VERSION} ${HADOOP_HOME}
-
-# restore original path
-cd -
