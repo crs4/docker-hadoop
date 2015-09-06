e9c68a2a71fd267de292891ea55ae00b26715bf4
Marco Enrico Piras <kikkomep@crs4.it>
2015-09-06 10:52:25 +0200

    Added log messages

--- images/hadoop/v2/scripts/init-folders.sh
+++ images/hadoop/v2/scripts/init-folders.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 # HDFS Folders
+echo -e "Initializing HDFS folders..."
+echo -e "  - creating /user/root"
 hdfs dfs -mkdir -p /user/root
+echo -e "  - creating /user/${UNPRIV_USER}"
 hdfs dfs -mkdir -p /user/${UNPRIV_USER}
+echo -e "  - setting ownership for /user/${UNPRIV_USER}"
 hdfs dfs -chown ${UNPRIV_USER} /user/${UNPRIV_USER}
+echo -e "  - creating /tmp"
 hdfs dfs -mkdir -p /tmp
+echo -e "  - setting permissions for /tmp"
 hdfs dfs -chmod 0777 /tmp
