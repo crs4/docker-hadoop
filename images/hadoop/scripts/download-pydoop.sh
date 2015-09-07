39d66a0619a7f4c65a7b0aa524e4ff297fcdc0a3
Marco Enrico Piras <kikkomep@crs4.it>
2015-09-07 20:53:18 +0200

    Removed install feature from download-pydoop script

--- images/hadoop/scripts/download-pydoop.sh
+++ images/hadoop/scripts/download-pydoop.sh
@@ -3,24 +3,8 @@
 PYDOOP_REPOSITORY="crs4/pydoop"
 PYDOOP_BRANCH="develop"
 
-if [[ "${1}" -eq "--skip-install" ]]; then  
-  if [[ "$#" -eq 3 ]]; then
-    PYDOOP_REPOSITORY="${2}"
-    PYDOOP_BRANCH="${3}" 
-  fi    
-else
-  if [[ "$#" -eq 2 ]]; then
-    PYDOOP_REPOSITORY="${1}"
-    PYDOOP_BRANCH="${2}" 
-  fi
-fi
+PYDOOP_REPOSITORY="${1}"
+PYDOOP_BRANCH="${2}" 
 
 # download pydoop
 git clone -b ${PYDOOP_BRANCH} https://github.com/${PYDOOP_REPOSITORY}
-
-# install pydoop
-if [[ "${1}" != "--skip-install" ]]; then
-  cd pydoop && \
-     python setup.py build && \
-     python setup.py install --skip-build
-fi
