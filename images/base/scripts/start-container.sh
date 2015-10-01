65eefe9db8a6b4b33d163c22a4d5796f1f29541f
Marco Enrico Piras <kikkomep@crs4.it>
2015-10-01 21:46:27 +0200

    Added param parser for the start-container script

--- images/base/scripts/start-container.sh
+++ images/base/scripts/start-container.sh
@@ -1,5 +1,50 @@
 #!/bin/bash
 
+# default values
+external_dns=false
+background_mode=false
+nfs_enabled=false
+nfs_shared_paths=""
+
+echo "$1 $2 $3"
+
+# print usage
+usage() {
+    echo "Usage: $0 -d [--external-dns] [--nfs-mounts]"
+    exit 1;
+}
+
+# parse arguments
+OPTS=`getopt -o :d --long external-dns,nfs-mounts: -n 'parse-options' -- "$@"`
+
+# check parsing result
+if [ $? != 0 ] ; then echo "Failed parsing options." >&2 ; usage; exit 1 ; fi
+
+# process arguments
+eval set -- "${OPTS}"
+while true; do
+  case "$1" in
+    -d ) background_mode="true"; shift ;;
+    --external-dns ) external_dns=true; shift;;
+    --nfs-mounts ) nfs_enabled="true"; nfs_shared_paths="${2}"; shift; shift ;;
+    -- ) shift; break ;;
+    * ) break ;;
+  esac
+done
+
+
+#  update the DNS table if needed
+if [[ ${external_dns} == true ]]; then
+	echo "- Using external DNS ..."
+else
+	update-etc-hosts
+fi
+
+
+# initializes shared folders
+init-shared-folders ${nfs_enabled} ${nfs_shared_paths}
+
+
 # set the existing ssh keys 
 shared_keys_path="/shared/keys"
 if [[ -d "${shared_keys_path}" ]]; then
