400386444eda9ebd8e50c133ad45e7b622c2d3a9
Marco Enrico Piras <kikkomep@crs4.it>
2015-10-03 16:03:43 +0000

    Fixed NFS client for waiting the corresponding server availability

--- images/base/scripts/nfs-client.sh
+++ images/base/scripts/nfs-client.sh
@@ -7,8 +7,11 @@ targets=()
 
 NFS_PORT_2049_TCP_ADDR="nfs.hadoop.docker.local"
 
-rpcbind
+# wait until
+echo "Waiting for NFS service availability..."
+while ! ping -c1 ${NFS_PORT_2049_TCP_ADDR} &>/dev/null; do :; done
 
+rpcbind
 for mnt in "${mounts[@]}"; do
   src=$(echo $mnt | awk -F':' '{ print $1 }')
   target=$(echo $mnt | awk -F':' '{ print $2 }')
