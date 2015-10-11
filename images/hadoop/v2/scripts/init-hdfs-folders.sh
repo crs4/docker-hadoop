f89c78810a27fa2f7a028ee0dc3de35bbe9f7c6f
Marco Enrico Piras <kikkomep@crs4.it>
2015-10-11 21:19:12 +0200

    Minor changes of the HDFS dir initialization

--- images/hadoop/v2/scripts/init-hdfs-folders.sh
+++ images/hadoop/v2/scripts/init-hdfs-folders.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo ${UNPRIV_USER}
+
 # HDFS Folders
 echo -e "Initializing HDFS folders..."
 echo -e "  - creating /user/root"
@@ -7,7 +9,13 @@ hdfs dfs -mkdir -p /user/root
 echo -e "  - creating /user/${UNPRIV_USER}"
 hdfs dfs -mkdir -p /user/${UNPRIV_USER}
 echo -e "  - setting ownership for /user/${UNPRIV_USER}"
-hdfs dfs -chown ${UNPRIV_USER} /user/${UNPRIV_USER}
+hdfs dfs -chown -R ${UNPRIV_USER}:${UNPRIV_USER} /user/${UNPRIV_USER}
+
+echo -e "  - creating /user/${USER}"
+hdfs dfs -mkdir -p /user/${USER}
+echo -e "  - setting ownership for /user/${UNPRIV_USER}"
+hdfs dfs -chown -R ${USER} /user/${USER}
+
 echo -e "  - creating /tmp"
 hdfs dfs -mkdir -p /tmp
 echo -e "  - setting permissions for /tmp"
