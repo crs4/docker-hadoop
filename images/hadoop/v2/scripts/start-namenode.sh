933ddd25afdc93e46f1758cc4ce395275e54d52e
Marco Enrico Piras <kikkomep@crs4.it>
2015-10-11 21:22:08 +0200

    Added '--update-hostname'  param; fixed init-hdfs launch

--- images/hadoop/v2/scripts/start-namenode.sh
+++ images/hadoop/v2/scripts/start-namenode.sh
@@ -3,16 +3,17 @@
 # default values
 background_mode=false
 nfs_enabled=false
+update_hostname=false
 nfs_shared_paths=""
 
 # print usage
 usage() {
-    echo "Usage: $0 [-d] [--nfs-mounts]"
+    echo "Usage: $0 [-d] [--nfs-mounts] [--update-hostname]"
     exit 1;
 }
 
 # parse arguments
-OPTS=`getopt -o :d --long nfs-mounts: -n 'parse-options' -- "$@"`
+OPTS=`getopt -o :d --long nfs-mounts:,update-hostname -n 'parse-options' -- "$@"`
 
 # check parsing result
 if [ $? != 0 ] ; then echo "Failed parsing options." >&2 ; usage; exit 1 ; fi
@@ -23,28 +24,35 @@ while true; do
   case "$1" in
     -d ) background_mode=true; shift ;;
     --nfs-mounts ) nfs_enabled=true; nfs_shared_paths="${2}"; shift; shift ;;
+    --update-hostname ) update_hostname=true; shift ;;
     -- ) shift; break ;;
     * ) break ;;
   esac
 done
 
 # update hostname
-update-hostname
+if [[ ${update_hostname} == true ]]; then
+    update-hostname
+fi
 
 # initializes shared folders
 init-shared-folders ${nfs_enabled} ${nfs_shared_paths}
 
 # Check whether the NameNode has been already formatted
+init_hdfs_folder=false;
 if [[ ! -d "${HDFS_DATA_DIR}" ]]; then
 	${HADOOP_HOME}/bin/hdfs namenode -format
+	init_hdfs_folder=true;
 fi
 
 # Start the NameNode
 ${HADOOP_HOME}/sbin/hadoop-daemon.sh --config $HADOOP_CONF_DIR start namenode
 
 # Init HDFS folders
-sleep 3
-${HADOOP_ADMIN_SCRIPTS}/init-hdfs-folders.sh
+if [[ ${init_hdfs_folder} == true ]]; then
+    sleep 3
+    ${HADOOP_ADMIN_SCRIPTS}/init-hdfs-folders.sh
+fi
 
 # Print logs in foreground mode if the first param is not '-d'
 if [[ ${background_mode} == false ]]; then
--- images/hadoop/v2/scripts/start-nodemanager.sh
+++ images/hadoop/v2/scripts/start-nodemanager.sh
@@ -3,16 +3,17 @@
 # default values
 background_mode=false
 nfs_enabled=false
+update_hostname=false
 nfs_shared_paths=""
 
 # print usage
 usage() {
-    echo "Usage: $0 [-d] [--nfs-mounts]"
+    echo "Usage: $0 [-d] [--nfs-mounts] [--update-hostname]"
     exit 1;
 }
 
 # parse arguments
-OPTS=`getopt -o :d --long nfs-mounts: -n 'parse-options' -- "$@"`
+OPTS=`getopt -o :d --long nfs-mounts:,update-hostname -n 'parse-options' -- "$@"`
 
 # check parsing result
 if [ $? != 0 ] ; then echo "Failed parsing options." >&2 ; usage; exit 1 ; fi
@@ -23,6 +24,7 @@ while true; do
   case "$1" in
     -d ) background_mode=true; shift ;;
     --nfs-mounts ) nfs_enabled=true; nfs_shared_paths="${2}"; shift; shift ;;
+    --update-hostname ) update_hostname=true; shift ;;
     -- ) shift; break ;;
     * ) break ;;
   esac
@@ -30,7 +32,9 @@ done
 
 
 # update hostname
-update-hostname
+if [[ ${update_hostname} == true ]]; then
+    update-hostname
+fi
 
 # initializes shared folders
 init-shared-folders ${nfs_enabled} ${nfs_shared_paths}
