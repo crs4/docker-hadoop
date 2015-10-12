ac21188fc5bac277dc066064cd7245715b88cc53
Marco Enrico Piras <kikkomep@crs4.it>
2015-10-12 19:47:30 +0200

    Added shared config file

---
+++ config.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# default user infos
+export DEFAULT_USER="hduser"
+export DEFAULT_USER_GROUP="hduser"
+export DEFAULT_USER_PASSWORD="hadoop"
+
+# compose project name
+DOCKER_COMPOSE_PROJECT="" #"${HADOOP_VERSION}"
